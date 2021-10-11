fc7042f0633075fe69692c27659c22950f753713
UFUK AYDAN <69341397+aydanufuk@users.noreply.github.com>
2021-10-11 15:44:35 +0300

    Add files via upload

---
+++ swig_struct_access_2/structStudy.h
@@ -0,0 +1,15 @@
+#pragma once
+#include<iostream>
+#include<conio.h>
+#include<stdlib.h>
+#include<stdio.h>
+
+struct demo_data
+{
+    int    x1;
+    char   s1[4][16];
+    char   s2[4][16];
+    char   s3[4][16];
+    int    x2[4];
+} demoStruct;
+
